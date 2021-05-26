a9c5251d80ac9d04b8a064fcfcd4efbf86563597
joaquinpco <joaquin1208@me.com>
2021-05-26 18:26:05 +0200

    Changed Cors policies

--- notes/notes.go
+++ notes/notes.go
@@ -40,6 +40,8 @@ func getClientFirestore() (context.Context, *firestore.Client) {
 
 func enableCors(w *http.ResponseWriter) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+    (*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+    (*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 }
 
 func Notes(w http.ResponseWriter, r *http.Request) {
